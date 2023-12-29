c751d9e25896fe24a5145ce9dd1d8991e08a5dcf
htrigga28 <holyworthakhigbe@gmail.com>
2023-12-29 22:54:40 +0100

    complete i think

--- src/routes/tasksRoutes.ts
+++ src/routes/tasksRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import Tasks from '../models/tasksModel';
 
 export default (router: Router) => {
+  // get all tasks
   router.get('/tasks', async (req, res) => {
     try {
       const tasks = await Tasks.find({});
@@ -11,6 +12,8 @@ export default (router: Router) => {
     }
     // res.send('Tasks');
   });
+
+  // create new task
   router.post('/tasks', async (req, res) => {
     try {
       const task = await Tasks.create(req.body);
@@ -19,6 +22,8 @@ export default (router: Router) => {
       res.status(500).json({ error: error.message });
     }
   });
+
+  // edit task description
   router.patch('/tasks/:taskId', async (req, res) => {
     const { title, description, done } = req.body;
     if (!title && !description && !done) {
@@ -34,6 +39,17 @@ export default (router: Router) => {
         }
       );
       res.status(201).json(task);
+    } catch (error: any) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
+  // delete task
+
+  router.delete('/tasks/:taskId', async (req, res) => {
+    try {
+      const task = await Tasks.deleteOne({ _id: req.params.taskId });
+      res.status(200).json(task);
     } catch (error: any) {
       res.status(500).json({ error: error.message });
     }
